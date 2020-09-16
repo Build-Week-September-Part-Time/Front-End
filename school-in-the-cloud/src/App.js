9476e349650a4fffa2c02316988cce1ef43c20e8
EmilyLow <emlow@me.com>
2020-09-16 18:47:53 -0500

    Route code in place

--- school-in-the-cloud/src/App.js
+++ school-in-the-cloud/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import {route} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+        <p>This is the App</p> 
+      <Router>
+        <Route  path="/login" component={Login} />
+        <Route  path="/signup" component={SignUp} />
+        {/* To be made PrivateRoute */}
+        <Route path="/student-home" component={StudentHome}/>
+        <Route path="/volunteer-home" component={VolunteerHome}/>
+        <Route path="/admin-hom" component={AdminHome}/>
+      </Router>
+
     </div>
   );
 }
