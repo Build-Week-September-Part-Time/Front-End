009943b49845c0b8badbc1d332b73a8ddbd0a924
EmilyLow <emlow@me.com>
2020-09-16 18:49:48 -0500

    Imports added

--- school-in-the-cloud/src/App.js
+++ school-in-the-cloud/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
+import StudentHome from "./components/student/StudentHome";
+import VolunteerHome from "./components/volunteer/VolunteerHome";
+import AdminHome from "./components/admin/AdminHome";
 
 function App() {
   return (
     <div>
         <p>This is the App</p> 
       <Router>
-        <Route  path="/login" component={Login} />
+        <Route  path="/login" component={LogIn} />
         <Route  path="/signup" component={SignUp} />
         {/* To be made PrivateRoute */}
         <Route path="/student-home" component={StudentHome}/>
