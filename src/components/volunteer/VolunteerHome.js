8f091324412a08d3eb433a7757cea1bc97d4e662
EmilyLow <emlow@me.com>
2020-09-17 20:46:23 -0500

    Voluteer structure and test data

--- src/components/volunteer/VolunteerHome.js
+++ src/components/volunteer/VolunteerHome.js
@@ -1,14 +1,43 @@
-import React from "react"; 
+import React, {useState} from "react"; 
+import VolunteerCard from "./VolunteerCard";
 
 function VolunteerHome() {
 
-
-    return(<div>
+    let [volunteers, setVolunteers] = useState(testVolunteers);
+    return(
+    <div>
         <p>Volunteer Home Page</p>
-
+        {volunteers.map((volunteer, i) => {
+             return <VolunteerCard volunteer={volunteer}/>
+        }    
+        )}
     </div>
         
    );
 }
 
 export default VolunteerHome;
+
+
+let testVolunteers = [
+    {
+        name: "Maria Avery",
+        email: "mAvery@gmail.com",
+        state: "Arizona",
+        availability: "Weekdays"
+    },
+    {
+        name: "Koli Woodsmith",
+        email: "KSmith@gmail.com",
+        state: "Rhode Island",
+        availability: "Weekends"
+    },
+    {
+        name: "Ursula K. LeGuin",
+        email: "UKLeGuin@gmail.com",
+        state: "Washington",
+        availability: "Everyday"
+    }
+
+
+];
