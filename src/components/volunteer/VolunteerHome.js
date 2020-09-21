8dd0a5993d21b6164ba75a05c72e394822c4cf17
EmilyLow <emlow@me.com>
2020-09-21 11:17:14 -0500

    Altered descriptive text

--- src/components/volunteer/VolunteerHome.js
+++ src/components/volunteer/VolunteerHome.js
@@ -9,7 +9,7 @@ function VolunteerHome() {
         <p>Volunteer Home Page</p>
 
         <h3>Volunteer List:</h3>
-        <p> Here temporarily, will be moved</p>
+        <p> Here temporarily, will be moved to student homepage</p>
         <VolunteerList />
     </div>
         
